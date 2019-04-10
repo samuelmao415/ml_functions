fdac79c73b54a34ef4eecd505600b8f742ae3194
samuelmao415 <samuelmao415@gmail.com>
2019-04-10 15:27:58 -0400

    script for python function

--- python_frequent_commands.py
+++ python_frequent_commands.py
@@ -1,2 +1,5 @@
 pd.set_option('max_colwidth', 200)
 pd.options.display.max_rows = 999
+
+
+toy.alltext=toy.alltext.apply(lambda x: re.sub("(\d+\%)+([a-z]+)|([a-z]+)+(\d+\%)", "\\2  \\2\\1  \\3  \\3\\4",x))
